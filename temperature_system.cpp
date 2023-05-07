8b4a902a0965e5cddfcafbc228a1e2a9f58874c6
SIDDHANTH V JAIN <95229520+Siddhanth09@users.noreply.github.com>
2023-05-07 11:43:46 +0530

    Add files via upload

---
+++ temperature_system.cpp
@@ -0,0 +1,42 @@
+#include "temperature_system.h"
+#include "Arduino.h"
+#include "main.h"
+
+
+void init_temperature_system(void)
+{
+    pinMode(HEATER, OUTPUT);
+    pinMode(COOLER, OUTPUT);
+
+    digitalWrite(HEATER,LOW);
+    digitalWrite(COOLER,LOW);
+    
+}
+
+float read_temperature(void)
+{
+   float temperature;
+
+   temperature =(((analogRead(TEMPERATURE_SENSOR) *(float)5/ 1024)) /(float) 0.01);
+   return temperature;
+}
+
+void cooler_control(bool control)
+{
+  if(control){
+    digitalWrite(COOLER ,1);
+  }
+  else{
+    digitalWrite(COOLER ,0);
+  }
+   
+}
+void heater_control(bool control)
+{
+    if(control){
+    digitalWrite(HEATER ,1);
+  }
+  else{
+    digitalWrite(HEATER,0);
+  } 
+}
